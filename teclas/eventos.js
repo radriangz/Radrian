91fe5579f5a5d737bf51aa191369401bb611b80e
Seth Karim Luis Martinez <radriangz@outlook.com>
2018-07-30 20:39:12 -0500

    Eventos del Teclado con JavaScript

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,6 +1,66 @@
-document.addEventListener("keyup", dibujarTeclado)
+
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+console.log(addEventListener);
+
+document.addEventListener("keyup", dibujarTeclado);
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujarLinea("red", x-1, y-1, x+1, x-1, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
 function dibujarTeclado(evento)
 {
-  console.log("tecla oprimida");
-  console.log(evento)
+  var colorcito = "blue";
+  var movimiento = 3;
+
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
+    break;
+    case teclas.LEFT:
+    dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+    x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+    dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+    x = x + movimiento;
+    break;
+  }
 }
+
+/*if(evento.keyCode == teclas.DOWN)
+{
+  console.log("Vamo' pá abajo");
+}
+if(evento.keyCode == teclas.LEFT)
+{
+  console.log("Vamo' pá la izquierda");
+}
+if(evento.keyCode == teclas.RIGHT)
+{
+  console.log("Vamo' pá la derecha");
+}*/
