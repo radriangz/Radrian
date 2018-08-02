4aedd8fc1b20af1b181d6237c1b79d3ba56044ae
Seth Karim Luis Martinez <radriangz@outlook.com>
2018-08-02 13:58:02 -0500

    funcion marco no funciona

--- teclas/dibujar_con_el_raton.js
+++ teclas/dibujar_con_el_raton.js
@@ -3,6 +3,7 @@ var lienzo = document.getElementById("area_de_dibujo");
 var papel = lienzo.getContext("2d");
 
 document.addEventListener("mousedown", dibujar);
+//document.addEventListener("mousemove") dibujar2);
 
 function dibujar(param)
 {
@@ -10,14 +11,26 @@ function dibujar(param)
   xi = param.layerX;
   yi = param.layerY;
   //alert(xi + ", " + yi);
-  dibujarLinea("black", xi, yi, 1, 499, papel, 3);
+    dibujarLinea("black", xi, yi, 1, 499, papel, 3);
 }
 
-dibujarLinea("gray", 1, 1, 1, 499, papel, 1);
-dibujarLinea("gray", 1, 1, 499, 1, papel, 1);
-dibujarLinea("gray", 1, 499, 499, 499, papel, 1);
-dibujarLinea("gray", 499, 1, 499, 499, papel, 1);
+function dibujar2()
+{
+  alert("funciona");
+}
 
+marco();
+
+function marco(dibujarLinea)
+{
+  a = 1;
+  b = 499;
+  marcoCol = "gray";
+  dibujarLinea("marcoCol", a, a, a, b, papel, 1);
+  dibujarLinea("marcoCol", a, a, b, a, papel, 1);
+  dibujarLinea("marcoCol", a, b, b, b, papel, 1);
+  dibujarLinea("marcoCol", b, a, b, b, papel, 1);
+}
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo, grosor)
 {
