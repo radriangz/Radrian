2891258ebfcf4eee933b7d24ab6246872a62c951
radriangz <radriangz@outlook.com>
2018-08-13 14:28:33 -0500

    Uncaught SyntaxError: Unexpected identifier js:35, el código funcionaba bien antes de agregar los pollos

--- Villa Platzi/platzi_villa.js
+++ Villa Platzi/platzi_villa.js
@@ -22,21 +22,31 @@ var lobo = {
 var vacas = {
   url:"vaca.png",
   cargaOK: false,
-  vaca1:0,
-  vaca2:0,
-  vaca3:0,
-  vaca4:0,
-  vaca5:0
+  vaca1: 0,
+  vaca2: 0,
+  vaca3: 0,
+  vaca4: 0,
+  vaca5: 0
 }
 
-var pollo = {
+var pollos = {
   url: "pollo.png",
   cargaOK: false
+  pollo1: 0,
+  pollo2: 0,
+  pollo3: 0,
+  pollo4: 0,
+  pollo5: 0
 }
 
-var cerdo = {
+var cerdos = {
   url: "cerdo.png",
-  cargaOK: false
+  cargaOK: false,
+  cerdo1:0,
+  cerdo2:0,
+  cerdo3:0,
+  cerdo4:0,
+  cerdo5:0
 }
 
 fondo.imagen = new Image();
@@ -51,13 +61,13 @@ vacas.imagen = new Image();
 vacas.imagen.src = vacas.url;
 vacas.imagen.addEventListener("load", cargarVacas);
 
-pollo.imagen = new Image();
-pollo.imagen.src = pollo.url;
-pollo.imagen.addEventListener("load", cargarPollos);
+pollos.imagen = new Image();
+pollos.imagen.src = pollo.url;
+pollos.imagen.addEventListener("load", cargarPollos);
 
-cerdo.imagen = new Image();
-cerdo.imagen.src = cerdo.url;
-cerdo.imagen.addEventListener("load", cargarCerdos);
+cerdos.imagen = new Image();
+cerdos.imagen.src = cerdo.url;
+cerdos.imagen.addEventListener("load", cargarCerdos);
 
 function cargarFondo() {
   fondo.cargaOK = true;
@@ -70,12 +80,12 @@ function cargarVacas() {
 }
 
 function cargarPollos() {
-  pollo.cargaOK = true;
+  pollos.cargaOK = true;
   dibujar();
 }
 
 function cargarCerdos() {
-  cerdo.cargaOK = true;
+  cerdos.cargaOK = true;
   dibujar();
 }
 
@@ -91,8 +101,6 @@ function dibujar(event) {
     fondo.dibujarFondo = true;
   }
   if(vacas.cargaOK & fondo.dibujarFondo) {
-    console.log( "x = " + vacas.vaca1.posicionX + ", y = " + vacas.vaca1.posicionY);
-    console.log("dibujando vacas");
     for(var v=0; v < 5; v++) {
       var vaca = {
         url: "vaca.png",
@@ -104,35 +112,60 @@ function dibujar(event) {
       if(v == 0) {
         if (vacas.vaca1 == 0) {
           vacas.vaca1 = vaca;
-          console.log("guardando vaca1: " + vacas.vaca1.posicionX + ", " + vacas.vaca1.posicionY);
         }
       }
       else if(v == 1) {
         vacas.vaca2 = vaca;
-        console.log("guardando vaca2: " + vacas.vaca2.posicionX + ", " + vacas.vaca2.posicionY);
       }
       else if(v == 2) {
         vacas.vaca3 = vaca;
-        console.log("guardando vaca3: " + vacas.vaca3.posicionX + ", " + vacas.vaca3.posicionY);
       }
       else if(v == 3) {
         vacas.vaca4 = vaca;
-        console.log("guardando vaca4: " + vacas.vaca4.posicionX + ", " + vacas.vaca4.posicionY);
       }
       else if (v == 4) {
         vacas.vaca5 = vaca;
-        console.log("guardando vaca5: " + vacas.vaca5.posicionX + ", " + vacas.vaca5.posicionY);
+        //console.log("guardando vaca5: " + vacas.vaca5.posicionX + ", " + vacas.vaca5.posicionY);
       }
       papel.drawImage(vacas.imagen, vaca.posicionX, vaca.posicionY);
     }
   }
-  if(pollo.cargaOK) {
+  if(pollos.cargaOK & fondo.dibujarFondo) {
     for(var v=0; v < 5; v++) {
       var x = aleatorio(0, 420);
       var y = aleatorio(0, 420);
-      papel.drawImage(pollo.imagen, x, y);
+
+      for(var v=0; v < 5; v++) {
+        var pollo = {
+          url: "pollo.png",
+          cargaOK: false,
+          posicionX: aleatorio(0,420),
+          posicionY: aleatorio(0,420),
+        }
+
+        if(v == 0) {
+          if (pollos.pollo1 == 0) {
+            pollos.pollo1 = pollo;
+          }
+        }
+        else if(v == 1) {
+          pollos.pollo2 = pollo;
+        }
+        else if(v == 2) {
+          pollos.pollo3 = pollo;
+        }
+        else if(v == 3) {
+          pollos.pollo4 = pollo;
+        }
+        else if (v == 4) {
+          pollos.pollo5 = pollo;
+          //console.log("guardando vaca5: " + vacas.vaca5.posicionX + ", " + vacas.vaca5.posicionY);
+        }
+        papel.drawImage(pollos.imagen, pollo.posicionX, pollo.posicionY);
+      }
     }
   }
+
   if(cerdo.cargaOK) {
       for(var v=0; v < 5; v++) {
         var x = aleatorio(0, 420);
@@ -146,11 +179,12 @@ function dibujar(event) {
 }
 
 function moverLobo(evento) {
-  console.log("vaca1: " + vacas.vaca1.posicionX + ", " + vacas.vaca1.posicionY);
+  /*console.log("vaca1: " + vacas.vaca1.posicionX + ", " + vacas.vaca1.posicionY);
   console.log("vaca2: " + vacas.vaca2.posicionX + ", " + vacas.vaca2.posicionY);
   console.log("vaca3: " + vacas.vaca3.posicionX + ", " + vacas.vaca3.posicionY);
   console.log("vaca4: " + vacas.vaca4.posicionX + ", " + vacas.vaca4.posicionY);
   console.log("vaca5: " + vacas.vaca5.posicionX + ", " + vacas.vaca5.posicionY);
+  */
 
   var teclas = {
     UP: 38,
@@ -194,6 +228,13 @@ function dibujarLoboSeMueve () {
       papel.drawImage(vacas.imagen, vacas.vaca4.posicionX, vacas.vaca4.posicionY);
       papel.drawImage(vacas.imagen, vacas.vaca5.posicionX, vacas.vaca5.posicionY);
   }
+  if(pollos.cargaOK) {
+      papel.drawImage(pollos.imagen, pollos.pollo1.posicionX, pollos.pollo1.posicionY);
+      papel.drawImage(pollos.imagen, pollos.pollo2.posicionX, pollos.pollo2.posicionY);
+      papel.drawImage(pollos.imagen, pollos.pollo3.posicionX, pollos.pollo3.posicionY);
+      papel.drawImage(pollos.imagen, pollos.pollo4.posicionX, pollos.pollo4.posicionY);
+      papel.drawImage(pollos.imagen, pollos.pollo5.posicionX, pollos.pollo5.posicionY);
+    }
   if(lobo.cargaOK) {
     papel.drawImage(lobo.imagen, lobo.loboX, lobo.loboY);
   }
