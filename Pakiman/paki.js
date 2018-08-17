80d688f1702a152119849b9b387820958fcb4254
radriangz <radriangz@outlook.com>
2018-08-17 16:34:57 -0500

    practica de clases y arrays en JavaScript completada

--- Pakiman/paki.js
+++ Pakiman/paki.js
@@ -3,25 +3,15 @@ imagenes["Cauchin"] = "vaca.png";
 imagenes["Pokacho"] = "pollo.png";
 imagenes["Tocinauro"] = "cerdo.png";
 
-class Pakiman {
-    constructor(n, v, a) {
-      this.imagen = new Image();
-      this.nombre = n;
-      this.vida = v;
-      this.ataque = a;
+var coleccion = [];
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman("Tocinauro", 120, 40));
 
-      this.imagen.src = imagenes[this.nombre];
-    }
-    hablar() {
-    }
-    mostrar()
-    {
-      document.appendChild(this.imagen);
-    }
+for(var pakin of coleccion) {
+  pakin.mostrar();
 }
 
-var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokacho", 80, 50);
-var tocinauro = new Pakiman("Tocinauro", 120, 40);
-
-pokacho.mostrar();
+for(var x of imagenes[0]) {
+  console.log(x);
+}
