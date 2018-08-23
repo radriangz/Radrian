e63ee7c70656ee3224860bc36a86fd55ce87715c
radriangz <radriangz@outlook.com>
2018-08-23 16:58:32 -0500

    codigo funcional, falta colocar los valores sobre la imagen, usando css

--- CalculadoraPdC/calculadora_pdc.js
+++ CalculadoraPdC/calculadora_pdc.js
@@ -2,22 +2,36 @@
 var clickIniciarCalculo = document.getElementById("iniciarCalculo")
 clickIniciarCalculo.addEventListener("click", calculaHiperfocal);
 
-var prueba = document.getElementById("distHiperfocal");
-prueba.innerHTML = "";
-
-var CIRCULOCONFUSION = 0.018; //constante exclusiva para canon apsc
+var distHiperfocal = document.getElementById("distHiperfocal");
 var hiperfocal;
+var cC;
+var distFocal;
+var fstop;
+var circuloConfusion = document.getElementById("circuloConfusion");
 var distanciasFocales = document.getElementById("distanciasFocales");
-distanciasFocales.addEventListener("change",  function(){
-    var distfocal = this.options[distanciasFocales.selectedIndex];
-    console.log("distanciasFocales = " + distanciasFocales.value);
-  });
-//alert("df = " + df.value);
+var aperturaDiaf = document.getElementById("aperturaDiaf");
 
-var aperturaDiaf;
+
+//Las funciones del addEventListener se pueden optimizar?
+circuloConfusion.addEventListener("change", function () {
+      cC = parseFloat(circuloConfusion.value);
+      console.log(cC);
+    });
+distanciasFocales.addEventListener("change", function () {
+      distFocal = parseInt(distanciasFocales.value);
+      console.log(distFocal);
+    });
+aperturaDiaf.addEventListener("change", function () {
+      fstop = parseFloat(aperturaDiaf.value);
+      console.log(fstop);
+    });
+
 
 function calculaHiperfocal () {
+  var hiperfocal = ((distFocal*distFocal)/(fstop*cC) + distFocal);
+  distHiperfocal.innerHTML = parseFloat(hiperfocal/1000).toFixed(2) + " m";
 
+  limiteHiperfocalCercano.innerHTML = parseFloat((hiperfocal/1000)/2).toFixed(1) + " m";
 
 
 }
