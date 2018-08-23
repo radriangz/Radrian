b57609f38724d5715b33cf73f786eac7d422d6b8
radriangz <radriangz@outlook.com>
2018-08-23 13:32:24 -0500

    el valor de distanciasfocales ya se establece, pero no puedo sacarlo de la función

--- CalculadoraPdC/calculadora_pdc.js
+++ CalculadoraPdC/calculadora_pdc.js
@@ -2,10 +2,22 @@
 var clickIniciarCalculo = document.getElementById("iniciarCalculo")
 clickIniciarCalculo.addEventListener("click", calculaHiperfocal);
 
-var prueba = document.getElementById("limiteHiperfocalCercano");
+var prueba = document.getElementById("distHiperfocal");
+prueba.innerHTML = "";
 
-prueba.innerHTML = "Texto de prueba";
+var CIRCULOCONFUSION = 0.018; //constante exclusiva para canon apsc
+var hiperfocal;
+var distanciasFocales = document.getElementById("distanciasFocales");
+distanciasFocales.addEventListener("change",  function(){
+    var distfocal = this.options[distanciasFocales.selectedIndex];
+    console.log("distanciasFocales = " + distanciasFocales.value);
+  });
+//alert("df = " + df.value);
+
+var aperturaDiaf;
 
 function calculaHiperfocal () {
-  alert("funca");
+
+
+
 }
