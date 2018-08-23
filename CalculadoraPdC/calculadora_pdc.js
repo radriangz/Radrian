aa452df3e43eed1f0fe089f90bc1196036480563
radriangz <radriangz@outlook.com>
2018-08-22 20:21:19 -0500

    Html terminado, js tiene un alert

---
+++ CalculadoraPdC/calculadora_pdc.js
@@ -0,0 +1,11 @@
+
+var clickIniciarCalculo = document.getElementById("iniciarCalculo")
+clickIniciarCalculo.addEventListener("click", calculaHiperfocal);
+
+var prueba = document.getElementById("limiteHiperfocalCercano");
+
+prueba.innerHTML = "Texto de prueba";
+
+function calculaHiperfocal () {
+  alert("funca");
+}
