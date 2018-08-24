91aa9566afa55f272fb1735f80eaec0ffedab3f6
radriangz <radriangz@outlook.com>
2018-08-23 21:49:37 -0500

    código funcional refactorizado, aún falta colocar etiquetas de texto sobre la imagen usando css

--- CalculadoraPdC/calculadora_pdc.js
+++ CalculadoraPdC/calculadora_pdc.js
@@ -4,31 +4,15 @@ clickIniciarCalculo.addEventListener("click", calculaHiperfocal);
 
 var distHiperfocal = document.getElementById("distHiperfocal");
 var circuloConfusion = document.getElementById("circuloConfusion");
-var distanciasFocales = document.getElementById("distanciasFocales");
-var aperturaDiaf = document.getElementById("aperturaDiaf");
-var hiperfocal, cC, distFocal, fstop;
-
-circuloConfusion.addEventListener("change", function () {
-      cC = parseFloat(circuloConfusion.value);
-      console.log(cC);
-    });
-
-distanciasFocales.addEventListener("change", function () {
-      distFocal = parseInt(distanciasFocales.value);
-      //console.log(distFocal);
-    });
-
-aperturaDiaf.addEventListener("change", function () {
-      fstop = parseFloat(aperturaDiaf.value);
-      //console.log(fstop);
-    });
+var distanciaFocal = document.getElementById("distanciasFocales");
+var aperturaDiafragma = document.getElementById("aperturaDiaf");
 
 function calculaHiperfocal () {
-  var hiperfocal = ((distFocal*distFocal)/(fstop*cC) + distFocal);
+  var hiperfocal = ((distanciaFocal.value*distanciaFocal.value)/(aperturaDiafragma.value*circuloConfusion.value) + distanciaFocal.value);
   console.log("hiperfocal: " + hiperfocal);
   //console.log("circuloConfusion: " + circuloConfusion.value);
 
-  if (isFinite(hiperfocal)) {
+  if (isFinite(hiperfocal) && (hiperfocal > 0)) {
     distHiperfocal.innerHTML = parseFloat(hiperfocal/1000).toFixed(2) + " m";
     limiteHiperfocalCercano.innerHTML =
     parseFloat((hiperfocal/1000)/2).toFixed(1) + " m";
