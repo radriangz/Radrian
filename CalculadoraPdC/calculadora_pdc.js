4571d8fbb173d8152cb9f64fe8b2b927caff6a3c
radriangz <radriangz@outlook.com>
2018-08-23 21:27:27 -0500

    código funcional, falta colocar texto sobre la imagen usando css

--- CalculadoraPdC/calculadora_pdc.js
+++ CalculadoraPdC/calculadora_pdc.js
@@ -3,43 +3,39 @@ var clickIniciarCalculo = document.getElementById("iniciarCalculo")
 clickIniciarCalculo.addEventListener("click", calculaHiperfocal);
 
 var distHiperfocal = document.getElementById("distHiperfocal");
-var hiperfocal;
-var cC;
-var distFocal;
-var fstop;
 var circuloConfusion = document.getElementById("circuloConfusion");
 var distanciasFocales = document.getElementById("distanciasFocales");
 var aperturaDiaf = document.getElementById("aperturaDiaf");
+var hiperfocal, cC, distFocal, fstop;
 
-
-//Las funciones del addEventListener se pueden optimizar?
 circuloConfusion.addEventListener("change", function () {
       cC = parseFloat(circuloConfusion.value);
       console.log(cC);
     });
+
 distanciasFocales.addEventListener("change", function () {
       distFocal = parseInt(distanciasFocales.value);
-      console.log(distFocal);
-    });
-aperturaDiaf.addEventListener("change", function () {
-      if (aperturaDiaf.value != 0) {
-      fstop = parseFloat(aperturaDiaf.value);
-      console.log(aperturaDiaf.value);
-      }
-      else {
-        alert("No deben quedar espacios sin seleccionar");
-      }
+      //console.log(distFocal);
     });
 
+aperturaDiaf.addEventListener("change", function () {
+      fstop = parseFloat(aperturaDiaf.value);
+      //console.log(fstop);
+    });
 
 function calculaHiperfocal () {
   var hiperfocal = ((distFocal*distFocal)/(fstop*cC) + distFocal);
-  console.log(hiperfocal);
+  console.log("hiperfocal: " + hiperfocal);
+  //console.log("circuloConfusion: " + circuloConfusion.value);
 
-  if (hiperfocal != NaN) {
+  if (isFinite(hiperfocal)) {
     distHiperfocal.innerHTML = parseFloat(hiperfocal/1000).toFixed(2) + " m";
-    limiteHiperfocalCercano.innerHTML = parseFloat((hiperfocal/1000)/2).toFixed(1) + " m";
+    limiteHiperfocalCercano.innerHTML =
+    parseFloat((hiperfocal/1000)/2).toFixed(1) + " m";
   }
   else {
+    alert("Hace falta elegir alguna función");
+    distHiperfocal.innerHTML = " ";
+    limiteHiperfocalCercano.innerHTML = " ";
   }
 }
